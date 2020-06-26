7cbd034b65c9d36e13480459867c642a13cabbc0
LanChi <lpham2525@gmail.com>
2020-06-26 12:58:39 -0700

    Updates

---
+++ precache-manifest.db64d239c583b960107c2e006fdb87c3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b27325f8d5814011bdab02c38b6f2a70",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "25cc40e733551863c125",
+    "url": "/portfolio/static/css/main.b80cd9ab.chunk.css"
+  },
+  {
+    "revision": "c2064c5d44f49538a0ba",
+    "url": "/portfolio/static/js/2.83f3327e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.83f3327e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25cc40e733551863c125",
+    "url": "/portfolio/static/js/main.5b76eed6.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "779a7d45fd34d9ba9867cf9841328176",
+    "url": "/portfolio/static/media/artist.779a7d45.png"
+  },
+  {
+    "revision": "47979beca1018bbd00c796d26a6b3f3c",
+    "url": "/portfolio/static/media/budget.47979bec.png"
+  },
+  {
+    "revision": "736463573ab462b50a7cf8a37ac0f5a2",
+    "url": "/portfolio/static/media/directory.73646357.png"
+  },
+  {
+    "revision": "5ff7f2c6aa02ecd3c59e68bfc4c922a4",
+    "url": "/portfolio/static/media/gem.5ff7f2c6.png"
+  },
+  {
+    "revision": "a87b4f4e8d5ee271c548028c2f8df31a",
+    "url": "/portfolio/static/media/pic.a87b4f4e.jpg"
+  },
+  {
+    "revision": "241fb3e00936e374e21de50812ad2146",
+    "url": "/portfolio/static/media/stats.241fb3e0.png"
+  },
+  {
+    "revision": "0da4c6107debabfbd6fb489c8e8764a0",
+    "url": "/portfolio/static/media/weather.0da4c610.png"
+  }
+]);
