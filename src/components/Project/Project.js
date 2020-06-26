8ef6338fe1ca812c224386130e654dc5550b751c
LanChi <lpham2525@gmail.com>
2020-06-26 10:05:18 -0700

    added 404.html and script to index.html

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -73,197 +73,37 @@ const useStyles = makeStyles({
   }
 })
 
-const Project = repos => {
-  console.log('repos props', repos)
+const Project = () => {
+  console.log('repos props =', repos)
   const classes = useStyles()
   return (
-  //     {props.repos.map(repo =>
-  //     <>
-  //       <Card className={classes.root}>
-  //         <CardActionArea>
-  //           <CardMedia
-  //             className={classes.media}
-  //             image={repos.name} alt='{repos.name}'
-  //           />
-  //           <CardContent>
-  //             <Typography gutterBottom variant="h5" component="h2">
-  //               {repos.name}
-  //             </Typography>
-  //             <Typography variant="body2" color="textSecondary" component="p">
-  //               {repos.summary}
-  //             </Typography>
-  //           </CardContent>
-  //         </CardActionArea>
-  //         <CardActions>
-  //           <Button size="small" color="primary" href='{repos.github}' target='_blank'>
-  //             GitHub Link
-  //           </Button>
-  //           <Button size="small" color="primary" href='{repos.app}' target='_blank'>
-  //             Deployed App
-  //           </Button>
-  //         </CardActions>
-  //       </Card>
-  //     </>
-  //   )
-  // }
     <>
-      <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image={gem}
-            alt='Little Gems screenshot'
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              Little Gems
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              "An app which points users to restaurants which have high ratings but are overlooked because they have sparse reviews.Using Google Maps, the app pinpoints the user's location and then leverages the user's input or keyword(s) to locate local eateries with ratings of 4 stars or higher but with fewer than 100 reviews.
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary" href='https://github.com/erikapaige/LittleGems/tree/LanChi' target='_blank'>
-            GitHub Link
-          </Button>
-          <Button size="small" color="primary" href='https://erikapaige.github.io/LittleGems/' target='_blank'>
-            Deployed App
-          </Button>
-        </CardActions>
-      </Card>
-
-      <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image={stats}
-            alt='Workout Tracker screenshot'
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              Workout Tracker
-                </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              An app which allows users to view, create, and track daily workouts. It also enables users to track the name, type, weight, sets, reps, and duration of resistance exercises. If the exercise is a cardio exercise, the workout tracker keeps track of the distance traveled. Last, the app includes charts and graphs of the workouts to give users a visual depiction of their progress.
-                </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary" href='https://github.com/lpham2525/workout_tracker' target='_blank'>
-            GitHub Link
-              </Button>
-          <Button size="small" color="primary" href='https://boiling-ocean-01580.herokuapp.com/' target='_blank'>
-            Deployed App
-              </Button>
-        </CardActions>
-      </Card>
-
-      
-
-      <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image={directory}
-            alt='user directory screenshot'
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              User Directory
-                </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              The User Directory app provides users a way to view non-sensitive data about the people in the directory. It allows users to sort the table by last name or job title as well as filter the directory by first or last name. The app is built with React and breaks up the application's UI into components, manages component state, and responds to user events.
-                </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary" href='https://github.com/lpham2525/directory' target='_blank'>
-            GitHub Link
-              </Button>
-          <Button size="small" color="primary" href="https://lpham2525.github.io/directory/" target='_blank'>
-            Deployed App
-              </Button>
-        </CardActions>
-      </Card>
-
-      <Card className={classes.root}>
+      {repos.map(repo =>
+        <Card className={classes.root} key={repo.id}>
           <CardActionArea>
             <CardMedia
               className={classes.media}
-              image={weather}
-              alt='Weather Dashboard screenshot'
+              image={repos.name} alt='{repos.name}'
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="h2">
-                Weather Dashboard
-                </Typography>
+                {repos.name}
+              </Typography>
               <Typography variant="body2" color="textSecondary" component="p">
-                An app where users can input the name of a city and receive weather information for that city. Information includes current temperature, humidity, wind speed, and UV index as well as a five-day forecast. Users can also save the city in their searches for convenient navigation and future use.
-                </Typography>
+                {repos.summary}
+              </Typography>
             </CardContent>
           </CardActionArea>
           <CardActions>
-            <Button size="small" color="primary" href='https://github.com/lpham2525/weatherdashboard' target='_blank'>
+            <Button size="small" color="primary" href='{repos.github}' target='_blank'>
               GitHub Link
-              </Button>
-            <Button size="small" color="primary" href='https://lpham2525.github.io/weatherdashboard/' target='_blank'>
+            </Button>
+            <Button size="small" color="primary" href='{repos.app}' target='_blank'>
               Deployed App
-              </Button>
+            </Button>
           </CardActions>
         </Card>
-
-      <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image={artist}
-            alt='Artist A Day screenshot'
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              Artist A Day
-                </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              "An app which provides consumers with a curated art shopping experience. Artists can upload their work and the app helps their art pieces stand out in the crowd. Each day features a new artist and the artist's work in a timed sale."
-          </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary" href='https://github.com/usrs/ArtistaDay' target='_blank'>
-            GitHub Link
-              </Button>
-          <Button size="small" color="primary" href='https://hidden-inlet-64215.herokuapp.com/' target='_blank'>
-            Deployed App
-              </Button>
-        </CardActions>
-      </Card>
-      
-      <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={budget}
-          alt='Budget Tracker screenshot'
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Budget Tracker
-            </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            An app in which users can add expenses and deposits to their budget with or without an internet connection. When entering transactions offline, the budget tracker will populate the total with the adjusted transactions when brought back online.
-            </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary" href='https://github.com/lpham2525/budget' target='_blank'>
-          GitHub Link
-          </Button>
-        <Button size="small" color="primary" href='https://guarded-spire-68532.herokuapp.com/' target='_blank'>
-          Deployed App
-          </Button>
-      </CardActions>
-    </Card>
+      )}
     </>
   )
 }
