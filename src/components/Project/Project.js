2486c736438ebbb61352248698e4091879844e25
LanChi <lpham2525@gmail.com>
2020-06-29 22:53:17 -0700

    tried fixing links and image routes

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -8,52 +8,52 @@ import CardMedia from '@material-ui/core/CardMedia'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import './Project.css'
-import artist from './artist.png'
-import gem from './gem.png'
-import directory from './directory.png'
-import weather from './weather.png'
-import stats from './stats.png'
-import budget from './budget.png'
+// import artist from './artist.png'
+// import gem from './gem.png'
+// import directory from './directory.png'
+// import weather from './weather.png'
+// import stats from './stats.png'
+// import budget from './budget.png'
 
 const repos = [{
   id: 1,
   name: 'Little Gems',
-  image: gem,
+  image: './gem.png',
   github: 'https://github.com/erikapaige/LittleGems/tree/LanChi',
   app: 'https://erikapaige.github.io/LittleGems/',
   summary: "An app which points users to restaurants which have high ratings but are overlooked because they have sparse reviews.Using Google Maps, the app pinpoints the user's location and then leverages the user's input or keyword(s) to locate local eateries with ratings of 4 stars or higher but with fewer than 100 reviews."
 }, {
   id: 2,
   name: 'Artist A Day',
-  image: artist,
+  image: './artist.png',
   github: 'https://github.com/usrs/ArtistaDay',
   app: 'https://hidden-inlet-64215.herokuapp.com/',
   summary: "An app which provides consumers with a curated art shopping experience. Artists can upload their work and the app helps their art pieces stand out in the crowd. Each day features a new artist and the artist's work in a timed sale."
 }, {
   id: 3,
   name: 'User directory',
-  image: directory,
+  image: './directory',
   github: 'https://github.com/lpham2525/directory',
   app: 'https://lpham2525.github.io/directory/',
   summary: "The User Directory app provides users a way to view non-sensitive data about the people in the directory. It allows users to sort the table by last name or job title as well as filter the directory by first or last name. The app is built with React and breaks up the application's UI into components, manages component state, and responds to user events."
 }, {
   id: 4,
   name: 'Weather Dashboard',
-  image: weather,
+  image: './weather',
   github: 'https://github.com/lpham2525/weatherdashboard',
   app: 'https://lpham2525.github.io/weatherdashboard/',
   summary: 'An app where users can input the name of a city and receive weather information for that city. Information includes current temperature, humidity, wind speed, and UV index as well as a five-day forecast. Users can also save the city in their searches for convenient navigation and future use.'
 }, {
   id: 5,
   name: 'Workout Tracker',
-  image: stats,
+  image: './stats',
   github: 'https://github.com/lpham2525/workout_tracker',
   app: 'https://boiling-ocean-01580.herokuapp.com/',
   summary: 'An app which allows users to view, create, and track daily workouts. It also enables users to track the name, type, weight, sets, reps, and duration of resistance exercises. If the exercise is a cardio exercise, the workout tracker keeps track of the distance traveled. Last, the app includes charts and graphs of the workouts to give users a visual depiction of their progress.'
 }, {
   id: 6,
   name: 'Budget Tracker',
-  image: budget,
+  image: './budget',
   github: 'https://github.com/lpham2525/budget',
   app: 'https://guarded-spire-68532.herokuapp.com/',
   summary: 'An app in which users can add expenses and deposits to their budget with or without an internet connection. When entering transactions offline, the budget tracker will populate the total with the adjusted transactions when brought back online.'
@@ -95,10 +95,10 @@ const Project = props => {
             </CardContent>
           </CardActionArea>
           <CardActions>
-            <Button size='small' color='primary' href='{repos.github}' target='_blank'>
+            <Button size='small' color='primary' href={repos.github} target='_blank'>
               GitHub Link
             </Button>
-            <Button size='small' color='primary' href='{repos.app}' target='_blank'>
+            <Button size='small' color='primary' href={repos.app} target='_blank'>
               Deployed App
             </Button>
           </CardActions>
