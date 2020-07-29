0ac9aeb061418513676cdc81a472fe20b6c3508e
LanChi <lpham2525@gmail.com>
2020-07-29 11:16:23 -0700

    fixed indent issues

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -264,24 +264,21 @@ const Project = repos => {
           <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
               Workout Tracker
-                </Typography>
+            </Typography>
             <Typography variant="body2" color="textSecondary" component="p">
               An app which allows users to view, create, and track daily workouts. It also enables users to track the name, type, weight, sets, reps, and duration of resistance exercises. If the exercise is a cardio exercise, the workout tracker keeps track of the distance traveled. Last, the app includes charts and graphs of the workouts to give users a visual depiction of their progress.
-                </Typography>
+            </Typography>
           </CardContent>
         </CardActionArea>
         <CardActions>
           <Button size="small" color="primary" href='https://github.com/lpham2525/workout_tracker' target='_blank'>
             GitHub Link
-              </Button>
+          </Button>
           <Button size="small" color="primary" href='https://boiling-ocean-01580.herokuapp.com/' target='_blank'>
             Deployed App
-              </Button>
+          </Button>
         </CardActions>
       </Card>
-
-
-
       <Card className={classes.root}>
         <CardActionArea>
           <CardMedia
@@ -292,19 +289,19 @@ const Project = repos => {
           <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
               User Directory
-                </Typography>
+            </Typography>
             <Typography variant="body2" color="textSecondary" component="p">
               The User Directory app provides users a way to view non-sensitive data about the people in the directory. It allows users to sort the table by last name or job title as well as filter the directory by first or last name. The app is built with React and breaks up the application's UI into components, manages component state, and responds to user events.
-                </Typography>
+            </Typography>
           </CardContent>
         </CardActionArea>
         <CardActions>
           <Button size="small" color="primary" href='https://github.com/lpham2525/directory' target='_blank'>
             GitHub Link
-              </Button>
+          </Button>
           <Button size="small" color="primary" href="https://lpham2525.github.io/directory/" target='_blank'>
             Deployed App
-              </Button>
+          </Button>
         </CardActions>
       </Card>
 
@@ -318,19 +315,19 @@ const Project = repos => {
           <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
               Weather Dashboard
-                </Typography>
+            </Typography>
             <Typography variant="body2" color="textSecondary" component="p">
               An app where users can input the name of a city and receive weather information for that city. Information includes current temperature, humidity, wind speed, and UV index as well as a five-day forecast. Users can also save the city in their searches for convenient navigation and future use.
-                </Typography>
+            </Typography>
           </CardContent>
         </CardActionArea>
         <CardActions>
           <Button size="small" color="primary" href='https://github.com/lpham2525/weatherdashboard' target='_blank'>
             GitHub Link
-              </Button>
+          </Button>
           <Button size="small" color="primary" href='https://lpham2525.github.io/weatherdashboard/' target='_blank'>
             Deployed App
-              </Button>
+          </Button>
         </CardActions>
       </Card>
 
@@ -344,19 +341,19 @@ const Project = repos => {
           <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
               Artist A Day
-                </Typography>
+            </Typography>
             <Typography variant="body2" color="textSecondary" component="p">
               "An app which provides consumers with a curated art shopping experience. Artists can upload their work and the app helps their art pieces stand out in the crowd. Each day features a new artist and the artist's work in a timed sale."
-          </Typography>
+            </Typography>
           </CardContent>
         </CardActionArea>
         <CardActions>
           <Button size="small" color="primary" href='https://github.com/usrs/ArtistaDay' target='_blank'>
             GitHub Link
-              </Button>
+          </Button>
           <Button size="small" color="primary" href='https://hidden-inlet-64215.herokuapp.com/' target='_blank'>
             Deployed App
-              </Button>
+          </Button>
         </CardActions>
       </Card>
 
