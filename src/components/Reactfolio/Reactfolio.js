5dd76356b369fa1129f8f9559b5146afaa48609a
LanChi <lpham2525@gmail.com>
2020-07-31 13:06:46 -0700

    removed useStyles from Reactfolio.js

--- src/components/Reactfolio/Reactfolio.js
+++ src/components/Reactfolio/Reactfolio.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 
 const Reactfolio = () => {
-  const classes = useStyles()
   return (
     <>
       <h1>React-folio Group Project</h1>
