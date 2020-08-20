2c1e32823c6b29fe3565e4cc91a5219beb345487
LanChi <lpham2525@gmail.com>
2020-08-20 10:54:32 -0700

    moved </Typography> to after the links on navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -28,7 +28,6 @@ const Navbar = () => {
     <AppBar position='static'>
       <Toolbar className='nav'>
         <Typography variant="h6" className={classes.title}>
-          <div>
           <Link to="/" color='inherit' className={classes.link} className='links'>
             Homepage
           </Link>
@@ -38,27 +37,25 @@ const Navbar = () => {
           <Link to='/Contact' color='inherit' className={classes.link} className='links'>
             Contact
           </Link>
-
-          {/* Icons */}
-          <div className='icon'>
-            <a href='https://www.linkedin.com/in/lanchi-pham/' target='_blank' rel='noopener noreferrer' id='linkedIn'>
-              <i className='fab fa-linkedin links' />
-            </a>
-
-            <a href='https://github.com/lpham2525' target='_blank' id='git' rel='noopener noreferrer'>
-              <i className='fab fa-github links' />
-            </a>
-
-            <a href='mailto:lpham2525@gmail.com? subject=subject text ' target='_blank' rel='noopener noreferrer' id='email-link'>
-              <i className='fas fa-envelope-square links' />
-            </a>
-
-            <a href='https://docs.google.com/document/d/1_eTe9DgBEbg6MghlNdJ14XX2KabCraCMirG6O505fH8/edit?usp=sharing' target='_blank' rel='noopener noreferrer' id='resume'>
-              <i className='far fa-file-alt links' />
-            </a>
-          </div>
-          </div>
         </Typography>
+        {/* Icons */}
+        <div className='icon'>
+          <a href='https://www.linkedin.com/in/lanchi-pham/' target='_blank' rel='noopener noreferrer' id='linkedIn'>
+            <i className='fab fa-linkedin links' />
+          </a>
+
+          <a href='https://github.com/lpham2525' target='_blank' id='git' rel='noopener noreferrer'>
+            <i className='fab fa-github links' />
+          </a>
+
+          <a href='mailto:lpham2525@gmail.com? subject=subject text ' target='_blank' rel='noopener noreferrer' id='email-link'>
+            <i className='fas fa-envelope-square links' />
+          </a>
+
+          <a href='https://docs.google.com/document/d/1_eTe9DgBEbg6MghlNdJ14XX2KabCraCMirG6O505fH8/edit?usp=sharing' target='_blank' rel='noopener noreferrer' id='resume'>
+            <i className='far fa-file-alt links' />
+          </a>
+        </div>
       </Toolbar>
     </AppBar>
   )
