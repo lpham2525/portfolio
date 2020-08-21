a8df04b31b218bbdce485c1fbf207e644ab9baf7
LanChi <lpham2525@gmail.com>
2020-08-21 11:56:55 -0700

    added in icon button

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import { Link } from 'react-router-dom'
@@ -25,6 +27,9 @@ const Navbar = () => {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar className='toolbar'>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
           <Typography variant="h6" className={classes.title}>
             <Link to="/" color='inherit' className={classes.link} className='links'>
               Homepage
